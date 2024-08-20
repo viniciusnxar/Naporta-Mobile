d5489896896ccac7020ddab5cd022fe9202254ae
Vinicius Noronha <96258593+viniciusnxar@users.noreply.github.com>
2024-08-20 20:02:55 -0300

    Tema da aplicação #11
    Criação da pasta themes que tem com as principais cores do app localizadas na index.ts. E o arquivo styled.d.ts está alterando o modulo styled-componets e transformando o theme em um Tipo

--- NaPorta/App.tsx
+++ NaPorta/App.tsx
@@ -3,15 +3,39 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
-export default function App() {
-  const [fonts] = [
-    useFonts({
-      Roboto_400Regular,
-    }),
-  ];
+import { ThemeProvider } from 'styled-components/native';
 
-  if (!fonts) {
+import theme from './src/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+  });
+
+  if (!fontsLoaded) {
     return <AppLoading />;
   }
-  return <View></View>;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello, World!</Text>
+        <StatusBar style='auto' />
+      </View>
+    </ThemeProvider>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.COLORS.BACKGROUND, // Exemplo de uso de theme diretamente
+  },
+  text: {
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 20,
+    backgroundColor: theme.COLORS.PRIMARY_900,
+  },
+});
---
+++ NaPorta/src/theme/index.ts
@@ -0,0 +1,29 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#F7F7F7',
+
+    PRIMARY_900: '#B83341',
+    PRIMARY_800: '#E03F50',
+    PRIMARY_100: '#D16470',
+    PRIMARY_50: '#FFABB3',
+
+    SECONDARY_900: '#572D31',
+    SECONDARY_500: '#7A6769',
+    SECONDARY_400: '#93797B',
+
+    SUCCESS_900: '#528F33',
+    SUCCESS_50: '#F7FFF9',
+
+    ALERT_900: '#B27F00',
+    ALERT_800: '#C5941A',
+    ALERT_50: '#F3EF5',
+
+    SHAPE: '#DCDCDC',
+    TITLE: '#FFF',
+    GRADIENT: ['#B83341', '#E03F50'],
+  },
+  FONTS: {
+    TITLE: 'Roboto_400Regular',
+    TEXT: 'Roboto_400Regular',
+  },
+};
