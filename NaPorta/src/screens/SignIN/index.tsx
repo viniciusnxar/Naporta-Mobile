68837c1ef376b624c8c3396eb9893970bb6fde36
Paulo Henrique <108034140+paulohenrique73@users.noreply.github.com>
2024-08-22 19:56:22 -0300

    Primeiros componentes de entrada #15

--- NaPorta/src/screens/SignIN/index.tsx
+++ NaPorta/src/screens/SignIN/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import {Container} from './styles'
 
+import { Input } from "@src/components/Input";
+
 export function SignIn(){
     return(
         <Container>
-            
+            <Input
+                placeholder="E-mail"
+                type="secondary"
+                autoCorrect={false}
+                autoCapitalize="none"
+            />
+
+            <Input
+                placeholder="Senha"
+                type="secondary"
+                secureTextEntry
+            />
         </Container> 
     );
 }
+
