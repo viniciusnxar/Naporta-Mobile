3ae02225f43468d2dd15ab9940c3f89e4e8a11f8
Vinicius Noronha <viniciusnxar@gmail.com>
2024-09-19 18:54:21 -0300

    Seção de Upload #51

--- NaPorta/src/screens/Product/index.tsx
+++ NaPorta/src/screens/Product/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import { Platform, TouchableOpacity } from 'react-native';
+import { Photo } from '@src/components/Photo';
+import { ButtonBack } from '@src/components/ButtonBack/index';
 
-import { Container, Header, Title, DeleteLabel } from './styles';
-import {ButtonBack} '@components/ButtonBack';
-
+import {
+  Container,
+  Header,
+  Title,
+  DeleteLabel,
+  Upload,
+  PickImageButton,
+} from './styles';
 export function Product() {
   return (
-    <Container
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-    >
-
+    <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
-          <ButtonBack />
-          <Title> Cadastrar </Title>
-          <TouchableOpacity>
-            <DeleteLabel> Deletar </DeleteLabel>
-          </TouchableOpacity>
+        <ButtonBack />
+        <Title> Cadastrar </Title>
+        <TouchableOpacity>
+          <DeleteLabel> Deletar </DeleteLabel>
+        </TouchableOpacity>
       </Header>
+      <Upload>
+        <Photo uri='' />
+        <PickImageButton title='Carregar' type='secondary' />
+      </Upload>
     </Container>
   );
 }
