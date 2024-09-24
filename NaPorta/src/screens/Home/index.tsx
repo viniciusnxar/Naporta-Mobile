903cdba4f7fe5405480f5fd75cc43d0b110ce542
Vinicius Noronha <167659791+OrdepOliveira@users.noreply.github.com>
2024-09-24 19:12:08 -0300

    Componente Search #83

--- NaPorta/src/screens/Home/index.tsx
+++ NaPorta/src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
 import { TouchableOpacity, Alert, FlatList } from 'react-native';
+import { Search } from '@src/components/Serach';
 import {
   Container,
   Header,
@@ -24,6 +25,7 @@ export function Home() {
           <MaterialIcons name='logout' color={COLORS.TITLE} size={24} />
         </TouchableOpacity>
       </Header>
+      <Search onSearch={() => {}} onClear={() => {}} />
     </Container>
   );
 }
