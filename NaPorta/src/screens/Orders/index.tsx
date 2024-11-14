752f2904b8b3365698d5dea7265ec3b701bc8dc5
Paulo Henrique <paulo.henrique71123@icloud.com>
2024-11-14 19:41:37 -0300

    Listar Pedidos #148

--- NaPorta/src/screens/Orders/index.tsx
+++ NaPorta/src/screens/Orders/index.tsx
@@ -1,12 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, FlatList } from 'react-native';
+import { FlatList } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 import { ItemSeparator } from '@components/ItemSeparator';
 
-import { OrderCard } from '@components/OrderCard';
+import { useAuth } from '@hooks/auth';
+import { OrderCard, OrderProps } from '@components/OrderCard';
 
 import { Container, Header, Title } from './styles';
 
 export function Orders() {
+  const [orders, setOrders] = useState<OrderProps[]>([]);
+  const { user } = useAuth();
+  useEffect(() => {
+    const subscribe = firestore()
+      .collection('orders')
+      .where('waiter_id', '==', user?.id)
+      .onSnapshot(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => {
+          return {
+            id: doc.id,
+            ...doc.data()
+          }
+        }) as OrderProps[];
+        setOrders(data);
+      });
+    return () => subscribe();
+  }, []);
   return (
     <Container>
       <Header>
@@ -15,13 +34,19 @@ export function Orders() {
 
       <FlatList
         data={['1', '2', '3']}
-        keyExtractor={(item) => item}
-        renderItem={({ item, index }) => <OrderCard index={index} />}
+        keyExtractor={item => item}
+        data={orders}
+        keyExtractor={item => item.id}
+        renderItem={({ item, index }) => (
+          <OrderCard index={index} />
+          <OrderCard index={index} data={item} />
+        )}
         numColumns={2}
         showsVerticalScrollIndicator={false}
         ItemSeparatorComponent={() => <ItemSeparator />}
         contentContainerStyle={{ paddingHorizontal: 24, paddingBottom: 125 }}
       />
+
     </Container>
   );
 }
