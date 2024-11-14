48207a3e8d93a70e4727500421ba703f9ac82a92
Vinicius Noronha <167659791+OrdepOliveira@users.noreply.github.com>
2024-11-14 12:39:59 -0300

    Componente de Pedido #140

---
+++ NaPorta/src/screens/Orders/index.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { Alert, FlatList } from 'react-native';
+import { ItemSeparator } from '@components/ItemSeparator';
+
+import { OrderCard } from '@components/OrderCard';
+
+import { Container, Header, Title } from './styles';
+
+export function Orders() {
+  return (
+    <Container>
+      <Header>
+        <Title>Pedidos feitos</Title>
+      </Header>
+
+      <FlatList
+        data={['1', '2', '3']}
+        keyExtractor={(item) => item}
+        renderItem={({ item, index }) => <OrderCard index={index} />}
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={() => <ItemSeparator />}
+        contentContainerStyle={{ paddingHorizontal: 24, paddingBottom: 125 }}
+      />
+    </Container>
+  );
+}
