8007996a5e0f23a2abebfd852dcab5240f881d69
Paulo Henrique <paulo.henrique71123@icloud.com>
2024-11-14 19:29:56 -0300

    Selecionando o preço #146

--- NaPorta/src/screens/Order/index.tsx
+++ NaPorta/src/screens/Order/index.tsx
@@ -63,9 +63,13 @@ type PizzaResponse = ProductProps & {
 export function Order() {
   const [size, setSize] = useState('');
   const [pizza, setPizza] = useState<PizzaResponse>({} as PizzaResponse);
+  const [quantity, setQuantity] = useState(0);
+  const [tableNumber, setTableNumber] = useState('');
   const navigation = useNavigation();
   const route = useRoute();
   const { id } = route.params as OrderNavigationProps;
+
+  const amount = size ? pizza.prices_sizes[size] * quantity : '0,00';
 
   function handleGoBack() {
     navigation.goBack();
@@ -108,15 +112,15 @@ export function Order() {
         <FormRow>
           <InputGroup>
             <Label>Número da mesa</Label>
-            <Input keyboardType='numeric' />
+            <Input keyboardType="numeric" onChangeText={setTableNumber} />
           </InputGroup>
 
           <InputGroup>
             <Label>Quantidade</Label>
-            <Input keyboardType='numeric' />
+            <Input keyboardType="numeric" onChangeText={(value) => setQuantity(Number(value))} />
           </InputGroup>
         </FormRow>
-        <Price>Valor de R$ 0 0</Price>
+        <Price>Valor de R$ {amount}</Price>
         <Button title='Confirmar pedido' />
       </Container>
     </ContentScroll>
